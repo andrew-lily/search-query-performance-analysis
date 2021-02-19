9b63d64c29bfff42cf086ae41ba97f25879736da
andrew-scott-nelson <andrew@lily.ai>
2021-02-19 12:48:45 -0600

    Error handling for search loop
    Just swallowing exceptions and continuing to iterate through the rest of the search terms

--- blm_test.py
+++ blm_test.py
@@ -39,7 +39,7 @@ args = sys.argv
 logging.basicConfig(level=logging.INFO)
 #now = datetime.now(timezone.utc)
 now = time.time()
-search_term_list = ['sheets']
+search_term_list = ['womanand #x27^;s ankle socks', 'sheets']
 shark_spinner = yaspin(Spinners.shark, color = "green")
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -59,37 +59,42 @@ if len(args) > 1:
     		search_term_list.append(row[0])
 #Create a new directory for scraped assests based on current time/day
 directory = str(now) + "/"
-parent_dir = "/Users/andrewnelson/Dev/site_search_POC/output/"
+parent_dir = "/Users/andrewnelson/Dev/site_search_POC/search-query-performance-analysis/output/"
 path = os.path.join(parent_dir, directory)
 os.mkdir(path)
 print(path)
 
 #Scraping image and utag data from each search term
 for x in search_term_list:
-    search_term = x
-    print(Fore.GREEN +"Searching for " + search_term)
-    shark_spinner.start()
-    driver = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver', options=options)
-    driver.get('https://www.bloomingdales.com/shop/search?keyword=' + search_term)
-    product_count = driver.find_element_by_class_name('page-range').text
-    screenshot = driver.get_screenshot_as_png()
-    #image = Image.open(StringIO.StringIO(screen))
-    png_src = BytesIO(screenshot)
-    png_data = Image.open(png_src)
-    rgb_png_data = png_data.convert('RGB')
-    rgb_png_data.save(path + search_term + "_" + str(now) + ".jpg", 'JPEG', optimize=True, quality=10)
-    #driver.save_screenshot(path + search_term + "_" + str(now) + ".png")
-    utag_data = driver.execute_script("return utag_data")
-    with open(path + 'search_data_' + str(now) +'.json', 'a+') as json_file:
-        json.dump(utag_data, json_file, indent=3)
-        json_file.write("\n")
-        #driver.find_element_by_css_selector("ul.grid-x>li>div>a").click();
-        #product_id = driver.find_element_by_css_selector("ul.grid-x>li>div").get_attribute("id")
-        #product_id_click = "css=#img_" + str(product_id) +  "> .alt-image > .thumbnailImage"
-        #print(product_id_click)
-        #driver.find_element_by_css_selector("css=#img_3312349>.alt-image>.thumbnailImage").click()
-        #time.sleep(2)
-    shark_spinner.stop()
-    print ("\033[A\033[A")
-    print(Fore.GREEN + 'The search term \"' + search_term + '\" returned ' + product_count)
-    driver.quit()
+    try:
+        search_term = x
+        print(Fore.GREEN +"Searching for " + search_term)
+        shark_spinner.start()
+        driver = webdriver.Chrome(executable_path=r'/usr/local/bin/chromedriver', options=options)
+        driver.get('https://www.bloomingdales.com/shop/search?keyword=' + search_term)
+        product_count = driver.find_element_by_class_name('page-range').text
+        screenshot = driver.get_screenshot_as_png()
+        #image = Image.open(StringIO.StringIO(screen))
+        png_src = BytesIO(screenshot)
+        png_data = Image.open(png_src)
+        rgb_png_data = png_data.convert('RGB')
+        rgb_png_data.save(path + search_term + "_" + str(now) + ".jpg", 'JPEG', optimize=True, quality=10)
+        #driver.save_screenshot(path + search_term + "_" + str(now) + ".png")
+        utag_data = driver.execute_script("return utag_data")
+        with open(path + 'search_data_' + str(now) +'.json', 'a+') as json_file:
+            json.dump(utag_data, json_file, indent=3)
+            json_file.write("\n")
+            #driver.find_element_by_css_selector("ul.grid-x>li>div>a").click();
+            #product_id = driver.find_element_by_css_selector("ul.grid-x>li>div").get_attribute("id")
+            #product_id_click = "css=#img_" + str(product_id) +  "> .alt-image > .thumbnailImage"
+            #print(product_id_click)
+            #driver.find_element_by_css_selector("css=#img_3312349>.alt-image>.thumbnailImage").click()
+            #time.sleep(2)
+        shark_spinner.stop()
+        print ("\033[A\033[A")
+        print(Fore.GREEN + 'The search term \"' + search_term + '\" returned ' + product_count)
+        driver.quit()
+    except Exception:
+        print(Fore.GREEN + "Something went wrong searching for" + search_term)
+        driver.quit()
+        continue
